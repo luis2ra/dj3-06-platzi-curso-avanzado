ed76cab26712d915a5907d46a35ae64bad369bfe
Luis <luis@blooz.pe>
2022-10-10 10:22:34 -0500

    how to run separate docker instance for debugging

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,19 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.contrib import admin
 
+'''
+esto es una prueba pa verificar el docker de django que
+se ejecuta por separado
+
+1. se mata la instancia de docker de django
+$ docker rm -f <ID>
+
+2. se levanta en otra consola la imagen de django
+$ docker-compose -f local.yml run --rm --service-ports django
+
+'''
+# import pdb; pdb.set_trace()
+
 urlpatterns = [
     # Django Admin
     path(settings.ADMIN_URL, admin.site.urls),
